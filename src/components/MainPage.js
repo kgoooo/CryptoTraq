920d0f3ff3c364f01c04eae7933a8db155f09a7e
Kristen Goehringer <kgoehringer@gmail.com>
2018-02-03 16:20:53 -0500

    Added MainPage to router to prepare for redistribution

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CryptoTraq from './CryptoTraq';
 
 class MainPage extends Component {
 	render(){
 		return(
 			<div>
 				<h1>Main PAGE</h1>
+				<CryptoTraq />
 			</div>
 		)
 	}
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
-import CryptoTraq from '../components/CryptoTraq';
+import MainPage from '../components/MainPage';
 import Header from '../components/Header';
 import Currencies from '../components/Currencies';
 
 
 const AppRouter = () => (
-	<BrowserRouter>
-		<div>
-			<Header />
-			<Route path="/" component={CryptoTraq} exact={true}/>
-			<Route path="/currencies" component={Currencies} />
-		</div>
-	</BrowserRouter>
-)
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Route path="/" component={MainPage} exact={true} />
+      <Route path="/currencies" component={Currencies} />
+    </div>
+  </BrowserRouter>
+);
 
 export default AppRouter;
