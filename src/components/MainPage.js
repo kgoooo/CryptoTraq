292a7108d66b42c90a3aea025cdfd04a5939b8eb
Kristen Goehringer <kgoehringer@gmail.com>
2018-02-04 13:49:16 -0500

    Starting to split API calls to seperate file - CryptoAPICalls

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import CryptoTraq from './CryptoTraq';
+import CryptoAPICalls from '../api/CryptoAPICalls';
 
 class MainPage extends Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+      cryptoList: [],
+      dontShow: true,
+      intlList: [],
+			isDisplayingGraph: false,
+      selectedCrypto: null,
+      selectedIntl: null,
+      showingExchangeRate: false,
+      showingExchangeError: false,
+      exchangeRate: 0,
+      dataSet: []
+    };
+	}
+	componentDidMount() {
+		CryptoAPICalls.onAppLoad()
+		// this.setState({
+    //   cryptoList: cryptoList,
+    //   intlList: intlList,
+    //   selectedCrypto: cryptoList[0].code,
+    //   selectedIntl: intlList[0].code
+    // });
+	}
+	
 	render(){
 		return(
 			<div>
 				<h1>Main PAGE</h1>
-				<CryptoTraq />
-			</div>
-		)
+				</div>
+			)
+		}
 	}
-}
+	// <CryptoTraq />
 
 export default MainPage;
