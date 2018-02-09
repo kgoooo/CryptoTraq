68e13cac2e5812f16507fe9a816a103d1443632f
Kristen Goehringer <kgoehringer@gmail.com>
2018-02-09 11:57:07 -0500

    finished refactor.

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -3,18 +3,32 @@ import React from 'react';
 const Currencies = (props) => {
 	const cryptoList = props.cryptoList;
 	const intlList = props.intlList;
-	return (
-		<div className="currencies">
-      <div className="currencies__container">
-        <ul className="currencies__list">
-          {cryptoList.map(c => <li className="currencies__item" key={c.code}>{c.name} - {c.code}</li>)}
-        </ul>
-        <ul className="currencies__list">
-          {intlList.map(i => <li className="currencies__item" key={i.code}>{i.name}</li>)}
-        </ul>
+	return <div className="currencies">
+      <div className="row">
+        <div className="col-1-of-2">
+          <div className="currencies--center">
+            <p className="currencies__label">Cryptocurrencies</p>
+            <ul className="currencies__list">
+              {cryptoList.map(c => (
+                <li className="currencies__item" key={c.code}>
+                  {c.name} - {c.code}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="col-1-of-2">
+          <div className="currencies--center">        
+            <p className="currencies__label">International currencies</p>
+            <ul className="currencies__list currencies__list--columns">
+              {intlList.map(i => <li className="currencies__item" key={i.code}>
+                  {i.name}
+                </li>)}
+            </ul>
+          </div>
+        </div>
       </div>
-		</div>
-	)
+    </div>;
 }
 
 export default Currencies;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-route
 
 
 const Header = (props) => {
-	return <div className="main__flex">
+	return (
+    <div className="main__flex">
       <div className="header">
         <h1 className="header__title" onClick={props.handleHideCurrencyList}>CryptoTraq</h1>
       </div>
@@ -12,8 +13,9 @@ const Header = (props) => {
         <button className="btn btn--light" onClick={props.handleShowCurrencyList}>
           Supported currencies
         </button>
-      </div>;
-    </div>;
+      </div>
+    </div>
+  )
 }
 
 export default Header;
